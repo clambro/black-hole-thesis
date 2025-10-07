e0c539e2ae6aef319365580807ad46301afbf9a5
clambro <clambro.git@gmail.com>
2025-10-06 17:39:58 -0700

    Update convergence_tests.sh

--- convergence_tests.sh
+++ convergence_tests.sh
@@ -1,9 +1,9 @@
 cargo build --release
-./target/release/black-hole-thesis --level-of-discretization 10
 ./target/release/black-hole-thesis --level-of-discretization 11
 ./target/release/black-hole-thesis --level-of-discretization 12
-python3 q_factor.py 30_10 30_11 30_12 radial_gradient
-python3 q_factor.py 30_10 30_11 30_12 conj_momentum
-python3 q_factor.py 30_10 30_11 30_12 energy_density
-python3 energy_conservation.py 30_10 30_11 30_12
-python3 initial_conditions.py 30_12
+./target/release/black-hole-thesis --level-of-discretization 13
+python3 q_factor.py 30_11 30_12 30_13 radial_gradient
+python3 q_factor.py 30_11 30_12 30_13 conj_momentum
+python3 q_factor.py 30_11 30_12 30_13 energy_density
+python3 energy_conservation.py 30_11 30_12 30_13
+python3 initial_conditions.py 30_13
