07600b9ca2c4398dc89a02fdf6ab53eb7a6d1afb
clambro <clambro.git@gmail.com>
2025-10-23 23:19:28 -0700

    Delete convergence_tests.sh

--- convergence_tests.sh
+++
@@ -1,9 +0,0 @@
-cargo build --release
-./target/release/black-hole-thesis --level-of-discretization 12
-./target/release/black-hole-thesis --level-of-discretization 13
-./target/release/black-hole-thesis --level-of-discretization 14
-uv --directory visualization run q_factor.py 30_12 30_13 30_14 field
-uv --directory visualization run q_factor.py 30_12 30_13 30_14 conj_momentum
-uv --directory visualization run q_factor.py 30_12 30_13 30_14 energy_density
-uv --directory visualization run check_convergence.py 30_12 30_13 30_14 --type energy
-uv --directory visualization run check_convergence.py 30_12 30_13 30_14 --type mass_residual
